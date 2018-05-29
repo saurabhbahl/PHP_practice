f4789e200afb30600ac9a36128649e838abac38c
VAIO PC <VAIO PC@VAIO.fritz.box>
2018-05-29 10:58:46 +0530

    `bvvcxvcxv

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-            echo "PHP_practice fgit";
+            echo "PHP_pradddddddddctice fgit";
             echo "PHP_practice git";
 
         ?>
